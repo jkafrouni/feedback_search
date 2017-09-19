a4767d43cd9f43594b88fa83d95cb7912be53d82
jkafrouni <j.kafrouni@gmail.com>
2017-09-18 23:17:17 -0400

    added stopwords removal

--- feedback_search/preprocess.py
+++ feedback_search/preprocess.py
@@ -1,7 +1,8 @@
 import re
 
-def split_remove_punctuation(text):
+STOPWORDS = open('resources/stopwords.txt').read().split('\n')
 
+def split_remove_punctuation(text):
     word_list = re.split('\W+', text) # split into words, without punctuation
     if '' in word_list: # quick fix, regex adds empty string
         word_list.remove('') 
@@ -10,5 +11,8 @@ def split_remove_punctuation(text):
     return word_list
 
 def remove_stopwords(word_list):
+    for w in STOPWORDS:
+        if w in word_list:
+            word_list.remove(w)
 
     return word_list
